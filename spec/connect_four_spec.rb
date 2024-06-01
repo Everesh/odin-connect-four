aba65f35e876d45238046dd463cc9e12af18c1ed
Everesh <everesh@proton.me>
2024-06-01 22:50:43 +0200

    Add tests for initializing current player var

--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -29,6 +29,28 @@ describe ConnectFour do
         expect(game.board).to all(have_attributes(length: described_class::BOARD_WIDTH))
       end
     end
+
+    it 'creates a instance variable called player' do
+      expect(game.instance_variable_get(:@player)).not_to be_nil
+    end
+
+    describe 'player' do
+      it 'has attr_reader method' do
+        expect(game).not_to respond_to(:player)
+      end
+
+      it 'has a private attr_accessor method' do
+        expect(game.private_methods).to include(:player).and include(:player=)
+      end
+
+      it 'is an integer' do
+        expect(game.instance_variable_get(:@player)).to be_an Integer
+      end
+
+      it 'is set to 1' do
+        expect(game.instance_variable_get(:@player)).to be(1)
+      end
+    end
   end
 
 end
