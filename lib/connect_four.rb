7b5c576056acb14eb1f4323d1f174d79789a568c
Everesh <everesh@proton.me>
2024-06-01 23:35:51 +0200

    Add play

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -9,7 +9,37 @@ class ConnectFour
     @player = 1
   end
 
+  def play
+    print_welcome
+    loop do
+      apply_move(get_input)
+      print_state
+      break if winner?
+
+      self.player = player == 1 ? 2 : 1
+    end
+    print_conclusion
+  end
+
   private
 
   attr_writer :board, :player
+
+  def print_welcome
+  end
+
+  def apply_move(column)
+  end
+
+  def get_input
+  end
+
+  def print_state
+  end
+
+  def winner?
+  end
+
+  def print_conclusion
+  end
 end
