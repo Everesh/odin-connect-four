532885ea2be4d64187f0b7d67bca926c22e9d631
Everesh <everesh@proton.me>
2024-06-01 22:43:17 +0200

    Implement initialize

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -1,4 +1,14 @@
 class ConnectFour
   BOARD_WIDTH = 7
   BOARD_HEIGHT = 6
+
+  attr_reader :board
+
+  def initialize
+    @board = Array.new(BOARD_HEIGHT) { Array.new(BOARD_WIDTH) { ' ' } }
+  end
+
+  private
+
+  attr_writer :board
 end
